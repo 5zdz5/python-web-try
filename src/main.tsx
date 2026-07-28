2aebbbc88a732403f8bfa9bff1862a01be27fbc8
5zdz5 <1069468682@qq.com>
2026-07-28 10:23:55 +0800

    chore: setup GitHub Pages deployment with HashRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { PyodideProvider } from './context/PyodideContext'
 import { ProgressProvider } from './context/ProgressContext'
 import App from './App'
@@ -10,9 +10,9 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ProgressProvider>
       <PyodideProvider>
-        <BrowserRouter>
+        <HashRouter>
           <App />
-        </BrowserRouter>
+        </HashRouter>
       </PyodideProvider>
     </ProgressProvider>
   </React.StrictMode>,
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/python-web-try/',
   server: {
     port: 3000,
     open: true
